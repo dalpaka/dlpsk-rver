011c5c5fa3d082ad6e41efacbd0fccfc3a7b78d6
River <83334985+RiverCheet@users.noreply.github.com>
2023-10-18 07:33:13 +0000

    farty

---
+++ fart.py
@@ -0,0 +1,5 @@
+fb = lambda x:("fizz" if x%3==0 else "")+("buzz" if x%5==0 else "")
+fb2 = (lambda ff,f,g,z:lambda c:(("\x66\x69"+((z)or(f)))if(g(((ff)or(f))-(0o2),((c)or(c))))else((("\x62u")or(f))+z)))(0o5,False,(lambda I,l:l%I==0),"\x7az")
+
+print(fb2(15))
+print([hex(ord(i)) for i in "zz"])
